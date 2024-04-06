a1f7415c23c8d316722cf7119abd9951e875a74d
Daniel Gurbin <daniel.g@msapps.mobi>
2024-04-06 15:37:15 +0300

    made basic login

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use ::mongodb::Client;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    web::{self, Data},
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use serde::Deserialize;
+use serde_json::json;
+
 mod mongodb;
 
 struct AppData {
@@ -20,16 +26,24 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(app_data.clone())
-            .route("/list_databases", web::get().to(list_databases))
+            .route("/auth/login", web::post().to(login))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-async fn list_databases(data: web::Data<AppData>) -> impl Responder {
-    match data.mongodb_client.list_database_names(None, None).await {
-        Ok(dbs) => HttpResponse::Ok().json(dbs),
-        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+#[derive(Debug, Deserialize)]
+struct ExpectedData {
+    password: String,
+}
+
+async fn login(body: web::Json<ExpectedData>, data: web::Data<AppData>) -> impl Responder {
+    let password = &body.password;
+    if password == "thisismypassword12" {
+        //*return token
+        HttpResponse::Ok().json(json!({"token":"this-is-your-token"}))
+    } else {
+        HttpResponse::BadRequest().body("this is not the password")
     }
 }
