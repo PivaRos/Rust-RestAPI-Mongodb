7d70b2d5ce0fab5fd79b961ca5e4e0ea923b3db2
Daniel Gurbin <daniel.g@msapps.mobi>
2024-04-06 14:53:15 +0300

    moved mongodb to its module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
+use ::mongodb::Client;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use mongodb::{options::ClientOptions, Client};
-use std::error::Error;
+mod mongodb;
 
-async fn get_mongodb_client(connection_string: &str) -> Result<Client, Box<dyn Error>> {
-    let client_options = ClientOptions::parse(connection_string).await?;
-    let client = Client::with_options(client_options)?;
-    Ok(client)
+struct AppData {
+    mongodb_client: Client,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mongodb_client = get_mongodb_client("mongodb://localhost:27017")
+    let mongodb_client = mongodb::get_mongodb_client("mongodb://localhost:27017")
         .await
         .expect("good");
 
+    let app_data = web::Data::new(AppData {
+        mongodb_client: mongodb_client.clone(),
+        // Add more data as needed
+    });
+
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(mongodb_client.clone()))
+            .app_data(app_data.clone())
             .route("/list_databases", web::get().to(list_databases))
     })
     .bind("127.0.0.1:8080")?
@@ -24,8 +27,8 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-async fn list_databases(data: web::Data<Client>) -> impl Responder {
-    match data.list_database_names(None, None).await {
+async fn list_databases(data: web::Data<AppData>) -> impl Responder {
+    match data.mongodb_client.list_database_names(None, None).await {
         Ok(dbs) => HttpResponse::Ok().json(dbs),
         Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
